ee715b9bfe188565d6cc3aadec65e810fa2cd49a
Max <donimassimo3@gmail.com>
2023-08-23 06:30:47 -0400

    b

--- backend/goal_achiver_api/models.py
+++ backend/goal_achiver_api/models.py
@@ -1,2 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class GoalsGroup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+        
+
+class Goal(models.Model):
+    title = models.TextField()
+    goals_group = models.ForeignKey(GoalsGroup, on_delete=models.CASCADE)
+    
+    class Meta:
+        abstract = True
+
+class CounterGoal(Goal):
+    counter = models.IntegerField(default=0)
+
+class LimitCounterGoal(Goal):
+    counter = models.IntegerField(default=0)
+    limit = models.IntegerField(default=0)
+
+class StarsGoal(Goal):
+    stars = models.IntegerField(default=0)
--- backend/goal_achiver_api/views.py
+++ backend/goal_achiver_api/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def start(request):
     print(request.data)
     return Response({"message": "Hello, world!"})
@@ -27,10 +30,16 @@ def sign_up(request):
 
 @api_view(['POST'])
 def login(request):
-    print(request.data)
-    user = get_object_or_404(User, username=[request.data['username']])
+    user = User.objects.get(username=request.data['username'])
+    print(user)
     if not user.check_password(request.data['password']):
         return Response({'detail':'password wrong'}, status=status.HTTP_404_NOT_FOUND)
-    token = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(isinstance=user)
-    return Response({'token':token.key, 'user':serilizer.data})
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    # if serializer.is_valid():
+    #     return Response({'token':token, 'user':serializer.data})
+    # else:
+    # return Response(serializer.errors)
+    return Response({'token':token.key, 'user':serializer.data})
+
+
